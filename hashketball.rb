f607b9b0ad06f65f2d7c9fe6343c5a4eada13869
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-09 18:24:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,40 @@
-# Write your code here!
+require 'pry'
+
+def game_hash
+  {
+    :home {
+      :team_name => ["Brooklyn Nets"],
+      :colors => ["Black", "White"],
+      :players => [{
+        :player_name => ""
+        :number => 
+        :shoe => 
+        :points => 
+        :rebounds => 
+        :assists => 
+        :steals => 
+        :blocks => 
+        :slam_dunks => }, 
+        {
+        :player_name => ""
+        :number => ""
+        :shoe => ""
+        :points => ""
+        :rebounds => ""
+        :assists => ""
+        :steals => ""
+        :blocks => ""
+        :slam_dunks => ""}, {:player_name => ""
+        :number => ""
+        :shoe => ""
+        :points => ""
+        :rebounds => ""
+        :assists => ""
+        :steals => ""
+        :blocks => ""
+        :slam_dunks => "" }, { }, { }, { }]
+        
+      
+    }
+  }
+end
