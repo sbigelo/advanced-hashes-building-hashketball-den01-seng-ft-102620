148bf08bb1c08542671bfbc7a788cee37823150f
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-09 18:33:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -46,26 +46,16 @@ def game_hash
         :blocks => 8
         :slam_dunks => 5}, 
         { 
-        :player_name => ""
-        :number => 
-        :shoe => 
-        :points => 
-        :rebounds => 
-        :assists => 
-        :steals => 
-        :blocks => 
-        :slam_dunks => }, 
-        { 
-        :player_name => ""
-        :number => 
-        :shoe => 
-        :points => 
-        :rebounds => 
-        :assists => 
-        :steals => 
-        :blocks => 
-        :slam_dunks =>
-        }]
+        :player_name => "Jason Terry"
+        :number => 31
+        :shoe => 15
+        :points => 19
+        :rebounds => 2
+        :assists => 2
+        :steals => 4
+        :blocks => 11
+        :slam_dunks => 1} 
+       ]
         
       
       
