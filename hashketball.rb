361099df90d8208d5649617e8db7bf9a014b7b6d
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-09 18:32:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,15 +36,15 @@ def game_hash
         :blocks => 1
         :slam_dunks => 15}, 
         { 
-          :player_name => ""
-        :number => 
-        :shoe => 
-        :points => 
-        :rebounds => 
-        :assists => 
-        :steals => 
-        :blocks => 
-        :slam_dunks => }, 
+          :player_name => "Mason Plumlee"
+        :number => 1
+        :shoe => 19
+        :points => 26
+        :rebounds => 11
+        :assists => 6
+        :steals => 3
+        :blocks => 8
+        :slam_dunks => 5}, 
         { 
         :player_name => ""
         :number => 
