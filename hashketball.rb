d726e43924612b7710c24a252d3c306d1f900064
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-09 18:42:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,18 +6,18 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [{
-        :player_name => "Alan Anderson"
-        :number => 0
-        :shoe => 16
-        :points => 22
-        :rebounds => 12
-        :assists => 12
-        :steals => 3
-        :blocks => 1
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
         :slam_dunks => 1}, 
         {
         :player_name => "Reggie Evans"
-        :number => 30
+        :number => 30,
         :shoe => 14
         :points => 12
         :rebounds => 12
