a5206857d0ff4e61b441bfec7623ca82e2ddd908
Sean Bigelow <sean.bigelow1@gmail.com>
2020-09-09 18:47:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,7 +61,7 @@ def game_hash
       
     },
       :away {
-      :team_name => "Charlotte Hornets",
+      team_name: => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [{
         :player_name => "Jeff Adrien",
